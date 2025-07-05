ac741b5e00ae20a1697ce70116d7161a1604b78b
Mayowa Sogbein <mayowa.sogbein@gmail.com>
2025-07-05 12:01:28 -0400

    update import

--- src/quizknit/flashcard.service.ts
+++ src/quizknit/flashcard.service.ts
@@ -1,5 +1,5 @@
 import OpenAI from "openai";
-import Flashcard from "../models/Flashcard.model";
+import Flashcard from "../models/flashcard.model";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
